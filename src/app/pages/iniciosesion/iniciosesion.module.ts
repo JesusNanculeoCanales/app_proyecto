84103b8e506ecfb8a12c4cc05c2eeab31cfe7855
Jesus Ñanculeo Canales <je.nanculeo@duocuc.cl>
2024-09-05 22:52:36 -0400

    Se realiza pruebas con page inicio de sesion

--- src/app/pages/iniciosesion/iniciosesion.module.ts
+++ src/app/pages/iniciosesion/iniciosesion.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { IniciosesionPageRoutingModule } from './iniciosesion-routing.module';
-
 import { IniciosesionPage } from './iniciosesion.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,  // Asegúrate de importar ReactiveFormsModule
     IonicModule,
     IniciosesionPageRoutingModule
   ],
--- src/app/pages/iniciosesion/iniciosesion.page.ts
+++ src/app/pages/iniciosesion/iniciosesion.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-iniciosesion',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./iniciosesion.page.scss'],
 })
 export class IniciosesionPage implements OnInit {
+  loginForm: FormGroup = this.fb.group({}); // Inicialización para evitar null
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
   }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulario válido', this.loginForm.value);
+    } else {
+      console.log('Formulario no válido');
+    }
+  }
 }
