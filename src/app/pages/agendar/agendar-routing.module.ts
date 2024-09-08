0834b37d23d0f46e52d0a633be14340fcaaf9cec
wala <wa.silva@duocuc.cl>
2024-09-08 03:59:09 -0300

    Agendar validaciones, jquery
    validaciones jquery y MAS

--- src/app/pages/agendar/agendar-routing.module.ts
+++ src/app/pages/agendar/agendar-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { AgendarPage } from './agendar.page';
 
 const routes: Routes = [
--- src/app/pages/agendar/agendar.module.ts
+++ src/app/pages/agendar/agendar.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { AgendarPageRoutingModule } from './agendar-routing.module';
-
 import { AgendarPage } from './agendar.page';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
-    ReactiveFormsModule,
+    ReactiveFormsModule,  // Asegúrate de importar ReactiveFormsModule si lo usas
     IonicModule,
     AgendarPageRoutingModule
   ],
